3a7f9421362dcfa85c2ce39a6458818d0a30af11
armaxro <arnaldomrojas@outlook.com>
2024-02-15 14:43:04 +0100

    index/model/vue

--- Modeles/informationsModel.php
+++ Modeles/informationsModel.php
@@ -1,15 +1,15 @@
 <?php
 
-function getInformations(PDO $db): array
+function getInformations(PDO $db)
 {
-    $sql = "SELECT * FROM themessage ORDER BY date_heure ASC";
+    $sql = "SELECT * FROM Informations ORDER BY thedate ASC";
     $query = $db->query($sql);
     $result = $query->fetchAll(PDO::FETCH_ASSOC);
     $query->closeCursor();
     return $result;
 }
 
-function addComments(PDO $db, string $themail, string $themessage): bool|string
+function addInformation(PDO $db, string $themail, string $themessage): bool|string
 {
     $themessage = htmlspecialchars(strip_tags(trim($themessage)), ENT_QUOTES);
     // false si le courriel n'est pas valide, sinon on le garde
@@ -21,7 +21,7 @@ function addComments(PDO $db, string $themail, string $themessage): bool|string
         return false;
     }
     // on prépare la requête
-    $sql = "INSERT INTO comments (courriel, texte) VALUES ('$themail', '$themail')";
+    $sql = "INSERT INTO informations (themail, themessage) VALUES ('$themail', '$themessage')";
     try {
         // on exécute la requête
         $db->exec($sql);
